3c6f8b0a293a22523bf2004fbde6f2caabb3f7d7
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-08-15 11:13:10 +0300

    0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # chance-in-a-raffle
+
+⠀UI to PY:
+```sh
+pyuic5 uis/main.ui -o windows/interface/main.py
+```
+
+⠀Build EXE:
+```sh
+pyinstaller app.py -wFn chance-in-a-raffle -i images/icons/app.ico --add-binary "images/icons;.images/icons"
+
+```
---
+++ windows/functionality/main.py
@@ -0,0 +1,50 @@
+from PyQt5 import QtGui, QtWidgets
+
+from utils.files import resource_path
+from windows.interface.main import Ui_main_window
+
+
+def comb(n, k):
+    if 0 <= k <= n:
+        nn = 1
+        kk = 1
+        for t in range(1, min(k, n - k) + 1):
+            nn *= n
+            kk *= t
+            n -= 1
+        return nn // kk
+    return 0
+
+
+class Window(QtWidgets.QWidget, Ui_main_window):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        # --- Window setting
+        self.setFixedSize(280, 145)
+        self.setWindowIcon(QtGui.QIcon(resource_path('.images/icons/app.ico')))
+
+        # --- Button signals
+        self.pushButton_calculate.clicked.connect(self.calculate)
+
+    def calculate(self):
+        try:
+            total = int(self.lineEdit_total.text().replace(' ', ''))
+            taken = int(self.lineEdit_taken.text().replace(' ', ''))
+            winning = int(self.lineEdit_winning.text().replace(' ', ''))
+
+            if taken == 1:
+                chance = winning / total * 100
+            else:
+                chance = (1 - comb(total - winning, taken) / comb(total, taken)) * 100
+
+            self.lineEdit_chance.setText(str(round(chance, 2)).replace('.', ',') + '%')
+
+        except ValueError:
+            QtWidgets.QMessageBox.critical(self, 'Error!', "One of the entered values isn't a number!",
+                                           QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
+
+        except:
+            QtWidgets.QMessageBox.critical(self, 'Error!', 'Something went wrong!', QtWidgets.QMessageBox.Ok,
+                                           QtWidgets.QMessageBox.Ok)
