f4f7f5ba1604be111ea1092f7c359c0c7d4162af
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-08-19 02:53:06 +0300

    Quote marks were changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PyQt5 import QtWidgets
 
 from windows.functionality.main import Window
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w = Window()
     w.show()
